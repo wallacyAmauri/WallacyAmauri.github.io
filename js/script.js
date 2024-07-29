19842205f55ac8fc124d8d83447104a869b0f5f6
WallacyAmauri <wallacya087@gmail.com>
2024-07-29 15:37:07 -0300

    1.0

--- js/script.js
+++ js/script.js
@@ -2,23 +2,22 @@ const header = document.querySelector("header");
 const hamburger = document.querySelector(".hamburger");
 const links = document.querySelectorAll(".nav-link");
 
-window.addEventListener("scroll", () => {
-    activeLink();
-    // if(!skillsPlayed) skillsCounter();
-    // if(!mlPlayed) mlCounty();
-    
+/* ----------- codigo do Navbar ----------- */
+
+function stickNavbar(){
+    header.classList.toggle("scrolled",window.scrollY > 0);
+ }
+ 
+ stickNavbar();
+ 
+ window.addEventListener("scroll", stickNavbar);
+ 
+/* ----------- abrir e fechar navbar menu ----------- */
+
+hamburger.addEventListener("click", () => {
+    document.body.classList.toggle("open");
+    document.body.classList.toggle("stopScrolling");
 });
-
-function updateCount(num, maxNum) {
-    let currentNum = +num.innerText;
-
-    if(currentNum < maxNum) {
-        num.innerText = currentNum + 1;
-        setTimeout(() => {
-            updateCount(num,maxNum);
-        }, 12);
-    }
-}
 
 /* ----------- scroll do link active ----------- */
 
@@ -32,59 +31,39 @@ function activeLink() {
     })
     .filter((sct) => sct.y <= 0);
     let currSectionID = passedSections.at(-1).id;
-
+    
     links.forEach((l) => l.classList.remove("active"));
     links[currSectionID].classList.add("active");
 } 
 activeLink();
-/* ----------- abrir e fechar navbar menu ----------- */
-
-hamburger.addEventListener("click", () => {
-    document.body.classList.toggle("open");
-    document.body.classList.toggle("stopScrolling");
-});
-
-// links.forEach((link) => 
-// link.addEventListener("click", () => {
-//     document.body.classList.remove("open");
-//     document.body.classList.remove("stopScrolling");
-// })
-// );
 
 
-/* ----------- codigo do Navbar ----------- */
-
-function stickNavbar(){
-    header.classList.toggle("scrolled",window.scrollY > 0);
- }
- 
- stickNavbar();
- 
- window.addEventListener("scroll", stickNavbar);
- 
-/* ----------- faq de serviços ----------- */
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        this.parentElement.classList.toggle("active");
-
-        var pannel = this.nextElementSibling;
-
-        if (pannel.style.display === "block") {
-            pannel.style.display = "none";
-        }else {
-            pannel.style.display = "block";
-        }
-    });
-}
-/* ----------- Digitilazação ----------- */
-const typed = new Typed(".text-home", {
-    strings: ["Frontend Developer", "Geolocalização","Social Midia","Trafego Pago", "Designer UI/Ux","Automação Web"],
-    typeSpeed: 90,
-    backSpeed: 100,
-    backDelay: 1000,
+/* ----------- swiper home ----------- */
+var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
   });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+  var swiper3 = new Swiper(".tuor", {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+    speed: 700,
+    loop: true,
+    rotate: true,
+    autoplay: {
+      delay: 700,
+      disableOnInteraction: false,
+    },
+    mousewheel: {
+      invert: false,
+    },
+});
